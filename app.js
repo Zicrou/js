ce01b9938327506c844a959f89ec490fc3f7ddc8
Zicrou <aazseck@gmail.com>
2021-04-30 09:55:52 +0000

    Added Data Types In Javascript

--- app.js
+++ app.js
@@ -1,48 +1,26 @@
-// // var, let, const
-// var name = "Fa Aziz";
-// console.log(name);
-// name = "FaAziz"
-// console.log(name)
+// PRIMITIVE
 
-// //Init
-// var greeting;
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
 
-//LET
-//let name;
-//name = "Fa Aziz";
-//console.log(name);
-//name = "FaAziz"
-//console.log(name)
-
-//CONST
-//can not do;
-const name = "Fa Aziz";
-console.log(name);
-//name = "FaAziz"
-//console.log(name)
-//Have to do: is assign a value
-const greeting = "Great";
-console.log(greeting);
-const person = {
-  name: "Aziz",
-  age: 28
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA'
 }
-console.log(person)
-
-console.log(person.name)
-console.log(person.age)
-
-person.age = 82
-console.log(person.age)
-
-person.name = "Fa"
-console.log(person.name)
-// Can not do
-// person = {
-//   color_skin: "black"
-// }
-// console.log(person.color_skin)
-const numbers = [1,2,3,4,5]
-console.log(numbers)
-numbers.push(6)
-console.log(numbers)
+const today = new Date();
+console.log(today);
+console.log(typeof today);
