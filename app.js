230dd8831b2b603f36c03e53e0bc5b6980a97a5e
Zicrou <aazseck@gmail.com>
2021-04-30 09:50:34 +0000

    Adde Variables Var, Let, Const

---
+++ app.js
@@ -0,0 +1,48 @@
+// // var, let, const
+// var name = "Fa Aziz";
+// console.log(name);
+// name = "FaAziz"
+// console.log(name)
+
+// //Init
+// var greeting;
+
+//LET
+//let name;
+//name = "Fa Aziz";
+//console.log(name);
+//name = "FaAziz"
+//console.log(name)
+
+//CONST
+//can not do;
+const name = "Fa Aziz";
+console.log(name);
+//name = "FaAziz"
+//console.log(name)
+//Have to do: is assign a value
+const greeting = "Great";
+console.log(greeting);
+const person = {
+  name: "Aziz",
+  age: 28
+}
+console.log(person)
+
+console.log(person.name)
+console.log(person.age)
+
+person.age = 82
+console.log(person.age)
+
+person.name = "Fa"
+console.log(person.name)
+// Can not do
+// person = {
+//   color_skin: "black"
+// }
+// console.log(person.color_skin)
+const numbers = [1,2,3,4,5]
+console.log(numbers)
+numbers.push(6)
+console.log(numbers)
